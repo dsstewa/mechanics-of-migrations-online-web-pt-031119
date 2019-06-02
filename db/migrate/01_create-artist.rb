79d40e48ed24796d5f39f12e28a06835fb94f459
David Stewart <davidstewart@gmail.com>
2019-06-02 19:00:34 +0000

    Automatically backed up by Learn

--- db/migrate/01_create-artist.rb
+++ db/migrate/01_create-artist.rb
@@ -1,22 +1,10 @@
-
 class CreateArtists < ActiveRecord::Migration
-    def up
-    
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
-   
-    def down
-     
-    end
-    
-    def change
-      create_table :artists do |t|
-        t.string :string
-        t.string :genre
-        t.integer :age
-        t.string :hometown
-      end
-    end
-    
-    
-    
+  end
 end
